74143a409a6dfc8c54b6375461400607f10046e4
kirill <kirill.bychkov@list.ru>
2022-05-11 19:37:21 +0700

    end

--- Program.cs
+++ Program.cs
@@ -6,6 +6,29 @@ namespace Parity_Check
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите число");
+            double a;
+            string b = Console.ReadLine();
+            try
+            {
+                a = Convert.ToDouble(b);
+                if (a % 2 == 0)
+                {
+                    Console.WriteLine("Число " + a + " четное");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.WriteLine("Число " + a + " нечетное");
+                    Console.ReadKey();
+                }
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("Ошибка, введено не число");
+                Console.ReadKey();
+            }
+
             
         }
     }
